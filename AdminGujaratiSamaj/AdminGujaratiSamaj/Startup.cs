67fd842b58d35fc6ba3a2e7b697ba7efbea542ee
vishaldhaduk <vishal.dhaduk11@gmail.com>
2016-05-24 07:23:09 -0400

    First commit after creating the project

---
+++ AdminGujaratiSamaj/AdminGujaratiSamaj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdminGujaratiSamaj.Startup))]
+namespace AdminGujaratiSamaj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
